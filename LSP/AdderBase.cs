49245a399b9a0a519f57b919371298d3a36a31bc
Damon German <damon.german@hyland.com>
2020-09-12 12:01:26 -0500

    Initial commit

---
+++ LSP/AdderBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LSP
+{
+    public class AdderBase
+    {
+        public virtual int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class BadAdder : AdderBase
+    {
+        public override int Add(int a, int b)
+        {
+            // violates the LSP
+            return a - b;
+        }
+    }
+
+    public class GoodAdder : AdderBase
+    {
+        public override int Add(int a, int b)
+        {
+            var fa = a + 0.0;
+            var fb = b + 0.0;
+            return (int) (fa + fb);
+        }
+    }
+
+    public class AnotherAdder : AdderBase
+    {
+        public override int Add(int a, int b)
+        {
+            // some other implmentation;
+            return a > b ? base.Add(a, b) : base.Add(b, a);
+        }
+    }
+}
---
+++ LSP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LSP
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var adders = new List<AdderBase>
+            {
+                new AdderBase(),
+                new GoodAdder(),
+                new BadAdder(),
+                new AnotherAdder()
+            };
+
+            PrintAdderResults(1, 1, adders);
+            PrintAdderResultsLSPViolation(5, 5, adders);
+        }
+
+        // Follows the OCP, DIP, assumes the LSP
+        private static void PrintAdderResults(int a, int b, IEnumerable<AdderBase> adders)
+        {
+            foreach (var adder in adders)
+            {
+                Console.WriteLine(adder.Add(a, b));
+            }
+        }
+
+        /// <summary>
+        /// If clients need to perform case analysis then your type violates the LSP
+        /// </summary>
+        private static void PrintAdderResultsLSPViolation(int a, int b, IEnumerable<AdderBase> adders)
+        {
+            foreach (var adder in adders)
+            {
+                // var bad = (BadAdder) adder;
+                if (adder is BadAdder) continue;
+
+                // var bad = adder as BadAdder;
+                // if(bad != null) continue;
+
+                Console.WriteLine(adder.Add(a, b));
+            }
+        }
+    }
+}
